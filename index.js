1aa986f9aa3f1c4129aad00971d03be9f5cfd81a
TemurMalik <tmaq77@gmail.com>
2026-03-14 14:58:31 +0500

    Push Revesion Code

---
+++ index.js
@@ -0,0 +1,19 @@
+/////////////////////////////////// React 3 Revesion ///////////////////////////////////
+
+// Redux nima => State Globalniy boshqarish uchun kutubxona
+
+// Store => statelarni saqlovchi markaz (omborxona)
+
+// action => bajariladigon ish harakat turi
+
+// useDispatch => action reducerga yubaradigan hook
+
+// useSelector => Storadan state oladi
+
+// Reducer => Asosiy funksiya .
+
+// Library va framework farqi ?
+
+// Library => js kutubxonasi kerakli packegelar ozida bolmaydi yuklab kelish kerka
+
+// fremework js fremeworki kerakli packegelar ozida boladi yuklab kelish kerak emas
